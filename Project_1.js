44ae0476640af4975b160a22a0b34aad4bbcb57a
Colin Au <colincau@gmail.com>
2025-04-21 21:10:18 -0700

    Added edits

--- Project_1.js
+++ Project_1.js
@@ -5,8 +5,9 @@ console.log("Hello,", name, "welcome to the game!");
 
 const shouldweplay = prompt("Do you want to play now? ");
 
+// Game Logic
 if (shouldweplay.toLowerCase() === "yes") {
-  // Game Logic
+  
   const leftOrRight = prompt(
     "You're now in a maze. Do you want to go left or right? "
   );
@@ -15,13 +16,13 @@ if (shouldweplay.toLowerCase() === "yes") {
     const cross = prompt("Do you want to cross the bridge? ");
     if (cross.toLowerCase() === "yes") {
       console.log(
-        "You've crossed the bridge, but the bridge was weak & the bridge broke. You then fell off the bridge. You lost the game."
+        "You've crossed the bridge, but the bridge was weak & the bridge broke. You then fell off the bridge. You lost the game. :("
       );
     } else {
-      console.log("Good choice.. You win the game!");
+      console.log("Good choice.. You win the game! Ya! :)");
     }
   } else {
-    console.log("You go right & fall off a cliff...");
+    console.log("You go right & fall off a cliff... :(");
   }
 } else if (shouldweplay.toLowerCase() === "no") {
   console.log("Okay :(");
