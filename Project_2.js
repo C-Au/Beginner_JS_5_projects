2b28409a6ec2298b5419b975d7078ffb4d01b26c
Colin Au <44826637+C-Au@users.noreply.github.com>
2025-04-22 20:53:52 -0700

    New File created: 2nd Project: JS Calculator App

---
+++ Project_2.js
@@ -0,0 +1,6 @@
+// JS Calculator App
+const prompt = require("prompt-sync")()
+
+const number1 = prompt("Please enter Number 1: ")
+const number2 = prompt("Please enter Number 2: ")
+const operator = prompt("Enter Sign: ")
