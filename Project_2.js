97d3029698da364f9d2b2b0952d8061020fb5cd5
Colin Au <44826637+C-Au@users.noreply.github.com>
2025-04-23 18:55:15 -0700

    1) Colin's Notes have been deleted; don't need them in the repo
    2) Updated JS Project 2; add more lines of code

--- Project_2.js
+++ Project_2.js
@@ -1,6 +1,30 @@
 // JS Calculator App
-const prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")();
 
-const number1 = prompt("Please enter Number 1: ")
-const number2 = prompt("Please enter Number 2: ")
-const operator = prompt("Enter Sign: ")
+const number1 = parseFloat(prompt("Please enter Number 1: "));
+const number2 = parseFloat(prompt("Please enter Number 2: "));
+const operator = prompt("Enter Sign: ");
+
+let result;
+let valid = true;
+switch (operator) {
+  case "+":
+    result = number1 + number2;
+    break;
+  case "-":
+    result = number1 - number2;
+    break;
+  case "/":
+    result = number1 / number2;
+    break;
+  case "*":
+    result = number1 * number2;
+    break;
+  default:
+    console.log("Invalid");
+    valid = false;
+    break;
+}
+
+if (valid) console.log(number1, operator, number2, "=", result);
+
