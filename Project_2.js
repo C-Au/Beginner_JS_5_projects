0dee2a4390a329318fdac4c807148240dd52e8c0
Colin Au <44826637+C-Au@users.noreply.github.com>
2025-05-15 20:00:58 -0700

    Added while loop to Project 2

--- Project_2.js
+++ Project_2.js
@@ -2,8 +2,27 @@
 // https://youtu.be/kZMwrXyxQYE?si=1gw5TUQ-W0Pd9I6x
 const prompt = require("prompt-sync")();
 
-const number1 = parseFloat(prompt("Please enter Number 1: "));
-const number2 = parseFloat(prompt("Please enter Number 2: "));
+let number1;
+let number2;
+
+while (true) {
+  number1 = parseFloat(prompt("Please enter Number 1: "));
+  if (isNaN(number1)) {
+    console.log("Invalid input");
+  } else {
+    break;
+  }
+}
+
+while (true) {
+  number2 = parseFloat(prompt("Please enter Number 2: "));
+  if (isNaN(number2)) {
+    console.log("Invalid input");
+  } else {
+    break;
+  }
+}
+
 const operator = prompt("Enter Sign: ");
 
 let result;
@@ -26,5 +45,5 @@ switch (operator) {
     valid = false;
     break;
 }
- 
+
 if (valid) console.log(number1, operator, number2, "=", result);
