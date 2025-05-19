8e53d267fd4755022e868dbea3525e45a3959dcb
Colin Au <44826637+C-Au@users.noreply.github.com>
2025-05-19 15:14:13 -0700

    Added contact functions with while loop/boolean

--- Project_3.js
+++ Project_3.js
@@ -1,16 +1,49 @@
 // JS Contact List App
 // https://youtu.be/kZMwrXyxQYE?si=1gw5TUQ-W0Pd9I6x
 
-const prompt = require("prompt-sync")()
+const prompt = require("prompt-sync")();
 
-function printInfo(){
-    console.log("Contact Management System")
-    console.log("-------------------------")
-    console.log("1. Add a Contact")
-    console.log("2. Delete a Contact")
-    console.log("3. View Contacts")
-    console.log("4. Search Contacts")
-    console.log("5. Exit")
+function printInfo() {
+  console.log("Contact Management System");
+  console.log("-------------------------");
+  console.log("1. Add a Contact");
+  console.log("2. Delete a Contact");
+  console.log("3. View Contacts");
+  console.log("4. Search Contacts");
+  console.log("5. Exit");
 }
 
-printInfo()
+function addContact() {}
+
+function deleteContact() {}
+
+function searchContact() {}
+
+function listContact(contacts) {}
+
+printInfo();
+
+let keepGoing = true;
+while (true) {
+  const number = prompt("Enter an operation (1-5): ");
+  switch (number) {
+    case "1":
+        addContact()
+      break;
+    case "2":
+        deleteContact()
+      break;
+    case "3":
+        listContact() // pass contacts here
+      break;
+    case "4":
+        searchContact()
+      break;
+    case "5":
+      keepGoing = false;
+      break;
+    default:
+      console.log("Unknown.");
+      break;
+  }
+}
