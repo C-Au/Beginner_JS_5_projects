253eb5aafee8f6772862931632137855db3d5eaf
Colin Au <44826637+C-Au@users.noreply.github.com>
2025-05-19 15:24:27 -0700

    example of how to use an array in JavaScript

--- Project_3.js
+++ Project_3.js
@@ -1,49 +1,53 @@
 // JS Contact List App
 // https://youtu.be/kZMwrXyxQYE?si=1gw5TUQ-W0Pd9I6x
 
-const prompt = require("prompt-sync")();
+// const prompt = require("prompt-sync")();
 
-function printInfo() {
-  console.log("Contact Management System");
-  console.log("-------------------------");
-  console.log("1. Add a Contact");
-  console.log("2. Delete a Contact");
-  console.log("3. View Contacts");
-  console.log("4. Search Contacts");
-  console.log("5. Exit");
-}
+// function printInfo() {
+//   console.log("Contact Management System");
+//   console.log("-------------------------");
+//   console.log("1. Add a Contact");
+//   console.log("2. Delete a Contact");
+//   console.log("3. View Contacts");
+//   console.log("4. Search Contacts");
+//   console.log("5. Exit");
+// }
 
-function addContact() {}
+// function addContact() {}
 
-function deleteContact() {}
+// function deleteContact() {}
 
-function searchContact() {}
+// function searchContact() {}
 
-function listContact(contacts) {}
+// function listContact(contacts) {}
 
-printInfo();
+// printInfo();
 
-let keepGoing = true;
-while (true) {
-  const number = prompt("Enter an operation (1-5): ");
-  switch (number) {
-    case "1":
-        addContact()
-      break;
-    case "2":
-        deleteContact()
-      break;
-    case "3":
-        listContact() // pass contacts here
-      break;
-    case "4":
-        searchContact()
-      break;
-    case "5":
-      keepGoing = false;
-      break;
-    default:
-      console.log("Unknown.");
-      break;
-  }
-}
+// let keepGoing = true;
+// while (true) {
+//   const number = prompt("Enter an operation (1-5): ");
+//   switch (number) {
+//     case "1":
+//         addContact()
+//       break;
+//     case "2":
+//         deleteContact()
+//       break;
+//     case "3":
+//         listContact() // pass contacts here
+//       break;
+//     case "4":
+//         searchContact()
+//       break;
+//     case "5":
+//       keepGoing = false;
+//       break;
+//     default:
+//       console.log("Unknown.");
+//       break;
+//   }
+// }
+
+const arr = [1, 2, 3, 4];
+arr[0] = 9;
+console.log(arr[1]);
